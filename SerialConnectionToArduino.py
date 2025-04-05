b593cf043e8fab69dd38391d2e642749d1f45936
AleBumbu <alexandrubumbu99@gmail.com>
2025-04-05 21:59:41 +0100

    Update SerialConnectionToArduino.py

--- SerialConnectionToArduino.py
+++ SerialConnectionToArduino.py
@@ -22,25 +22,11 @@ def rightAnswer(player):
     arduino.reset_input_buffer()
     arduino.write(f'win,{player}\n')
     # Wait a little bit for Arduino
-    time.sleep(3)
-
-    # Read response
-    if arduino.in_waiting > 0:
-        data = arduino.readline().decode().strip()
-        return data
-    else:
-        return "error, no response from Arduino"
+    time.sleep(0.2)
 
 def wrongAnswer(player):
     player = (player + 1) % 2
     arduino.reset_input_buffer()
     arduino.write(f'lose,{player}\n')
     # Wait a little bit for Arduino
-    time.sleep(3)
-
-    # Read response
-    if arduino.in_waiting > 0:
-        data = arduino.readline().decode().strip()
-        return data
-    else:
-        return "error, no response from Arduino"
+    time.sleep(0.2)
