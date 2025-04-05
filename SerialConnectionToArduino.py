e87b6187cbce1a5777b45dfea1847333484bb071
AleBumbu <alexandrubumbu99@gmail.com>
2025-04-05 21:50:55 +0100

    Added the connection to Arduino

---
+++ SerialConnectionToArduino.py
@@ -0,0 +1,46 @@
+import serial
+import time
+
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
+
+def get_answer():
+    arduino.reset_input_buffer()
+    arduino.write(b'answer_time\n')
+
+    # Wait for Arduino
+    time.sleep(3)
+
+    # Read response
+    if arduino.in_waiting > 0:
+        data = arduino.readline().decode().strip()
+        return data
+    else:
+        return "error, no response from Arduino"
+
+
+def rightAnswer(player):
+    arduino.reset_input_buffer()
+    arduino.write(f'win,{player}\n')
+    # Wait a little bit for Arduino
+    time.sleep(3)
+
+    # Read response
+    if arduino.in_waiting > 0:
+        data = arduino.readline().decode().strip()
+        return data
+    else:
+        return "error, no response from Arduino"
+
+def wrongAnswer(player):
+    player = (player + 1) % 2
+    arduino.reset_input_buffer()
+    arduino.write(f'lose,{player}\n')
+    # Wait a little bit for Arduino
+    time.sleep(3)
+
+    # Read response
+    if arduino.in_waiting > 0:
+        data = arduino.readline().decode().strip()
+        return data
+    else:
+        return "error, no response from Arduino"
